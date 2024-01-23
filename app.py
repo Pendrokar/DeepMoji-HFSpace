4c5f8db9e0ac2a24a751760b4907d2206680f5db
Yanis L <Pendrokar@users.noreply.huggingface.co>
2024-01-23 17:57:50 +0000

    revert

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 import gradio as gr
+import torch
 from transformers import pipeline, AutoTokenizer, DistilBertForSequenceClassification
 
 modelName = "colinryan/hf-deepmoji"
 
-distilTokenizer = AutoTokenizer.from_pretrained(modelName)
-distilTokenizer.save_pretrained("./model/")
-distilModel = DistilBertForSequenceClassification.from_pretrained(modelName, problem_type="multi_label_classification", num_labels=64)
-#distilModel = DistilBertForMultilabelSequenceClassification.from_pretrained("colinryan/hf-deepmoji")
+#distil_tokenizer = AutoTokenizer.from_pretrained(modelName)
+#distil_tokenizer.save_pretrained("./model/")
+#distil_model = DistilBertForSequenceClassification.from_pretrained(modelName, problem_type="multi_label_classification")
+#distil_model = DistilBertForMultilabelSequenceClassification.from_pretrained("colinryan/hf-deepmoji")
+#num_labels = len(model.config.id2label)
 
-pipeline = pipeline(task="text-classification", model=distilModel, tokenizer=tokenizer)
+#pipeline = pipeline(task="text-classification", model=distil_model, tokenizer=distil_tokenizer)
+pipeline = pipeline(task="text-classification", model=modelName)
 
 def predict(deepmoji_analysis):
     predictions = pipeline(deepmoji_analysis)

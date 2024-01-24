1ef1fd0aff64efc8e88f780ed24764c5dbf1c841
Yanis L <Pendrokar@users.noreply.huggingface.co>
2024-01-24 20:33:41 +0000

    Update app.py

--- app.py
+++ app.py
@@ -5,14 +5,9 @@ from transformers import pipeline, AutoTokenizer, DistilBertForSequenceClassific
 modelName = "Pendrokar/TorchMoji"
 
 distil_tokenizer = AutoTokenizer.from_pretrained(modelName)
-#distil_tokenizer.save_pretrained("./model/")
-#distil_model = DistilBertForSequenceClassification.from_pretrained(modelName, problem_type="multi_label_classification")
 distil_model = DistilBertForMultilabelSequenceClassification.from_pretrained(modelName)
-#num_labels = len(model.config.id2label)
 
-#pipeline = pipeline(task="text-classification", model=distil_model, tokenizer=distil_tokenizer)
 pipeline = pipeline(task="text-classification", model=distil_model, tokenizer=distil_tokenizer)
-#pipeline = pipeline(task="text-classification", model=modelName)
 
 def predict(deepmoji_analysis):
     predictions = pipeline(deepmoji_analysis)

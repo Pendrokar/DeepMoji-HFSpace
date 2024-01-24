9fb84ef085755a8cd1dd7969debb4adf585901b0
Yanis L <Pendrokar@users.noreply.huggingface.co>
2024-01-24 17:58:59 +0000

    multi

--- app.py
+++ app.py
@@ -4,10 +4,10 @@ from transformers import pipeline, AutoTokenizer, DistilBertForSequenceClassific
 
 modelName = "Pendrokar/TorchMoji"
 
-#distil_tokenizer = AutoTokenizer.from_pretrained(modelName)
+distil_tokenizer = AutoTokenizer.from_pretrained(modelName)
 #distil_tokenizer.save_pretrained("./model/")
-distil_model = DistilBertForSequenceClassification.from_pretrained(modelName, problem_type="multi_label_classification")
-#distil_model = DistilBertForMultilabelSequenceClassification.from_pretrained("colinryan/hf-deepmoji")
+#distil_model = DistilBertForSequenceClassification.from_pretrained(modelName, problem_type="multi_label_classification")
+distil_model = DistilBertForMultilabelSequenceClassification.from_pretrained("colinryan/hf-deepmoji")
 #num_labels = len(model.config.id2label)
 
 #pipeline = pipeline(task="text-classification", model=distil_model, tokenizer=distil_tokenizer)

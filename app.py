da98fdb6d78411dd22ee4ca7efa6a498ca6663a3
Yanis Lukes <Pendrokar@gmail.com>
2024-02-02 18:15:12 +0200

    ~/.cache

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from __future__ import print_function, division, unicode_literals
 import gradio as gr
 
 import sys
-from os import environ
 from os.path import abspath, dirname
 
 import json
@@ -15,7 +14,7 @@ from transformers import AutoModel, AutoTokenizer
 model_name = "Pendrokar/TorchMoji"
 model = AutoModel.from_pretrained(model_name)
 tokenizer = AutoTokenizer.from_pretrained(model_name)
-model_path = environ['TRANSFORMERS_CACHE'] + "/pytorch_model.bin"
+model_path = "~/.cache/huggingface/transformers/pytorch_model.bin"
 vocab_path = './' + model_name + "/vocabulary.json"
 
 def top_elements(array, k):

84c02ea2805ece1c9c8fe1850103038a3b081b7b
Yanis Lukes <Pendrokar@gmail.com>
2024-02-02 17:37:31 +0200

    back to model clone; uberduck's config is old

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import numpy as np
 from torchmoji.sentence_tokenizer import SentenceTokenizer
 from torchmoji.model_def import torchmoji_emojis
 from transformers import AutoModel
-model_name = "Uberduck/torchmoji"
+model_name = "Pendrokar/TorchMoji"
 model = AutoModel.from_pretrained(model_name)
 model_path = model
 vocab_path = './' + model_name + "/vocabulary.json"

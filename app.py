80f1d19de9daae579c7cbd06bb4a1cdce9cbbd1e
Yanis Lukes <Pendrokar@gmail.com>
2024-02-04 11:28:16 +0200

    python: trim => strip

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ st = SentenceTokenizer(vocabulary, maxlen)
 model = torchmoji_emojis(model_path)
 
 def predict(deepmoji_analysis, emoji_count):
-    if deepmoji_analysis.trim() == '':
+    if deepmoji_analysis.strip() == '':
         # dotted face emoji
         return {"🫥":1}
 
